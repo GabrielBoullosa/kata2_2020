9226761ff05133237c0d807808baf92b0d564b23
gabriel_hijo <gabriel_hijo@gabriel_hijo-PC.home>
2020-01-03 13:07:39 +0100

    I implements the HistogramDisplay class.

--- src/kata2_2020/HistogramDisplay.java
+++ src/kata2_2020/HistogramDisplay.java
@@ -1,4 +1,13 @@
 package kata2_2020;
 public class HistogramDisplay {
+    private final Histogram histogram;
+
+    public HistogramDisplay(Histogram histogram) {
+        this.histogram = histogram;
+    }
     
+    public void execute(){
+        for(int value: histogram.getValues())
+            System.out.println(value + " "+ histogram.getCounts(value));
+    }
 }
